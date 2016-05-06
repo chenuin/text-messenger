c09b385985107738a00cea286ea6a84f81ef2271
chenuin <yuyin0036@gmail.com>
2016-05-06 02:44:32 -0700

    login

--- client.py
+++ client.py
@@ -8,6 +8,17 @@ import sys
 import socket
 import getpass
 
+class color:
+    end =  "\033[0m"
+    black = "\033[30m"
+    red =   "\033[31m"
+    green = "\033[32m"
+    yellow ="\033[33m"
+    blue =  "\033[34m"
+    purple ="\033[35m"
+    cyan =  "\033[36m"
+    grey =  "\033[37m"
+
 def strDecode(string):
     bytes_str = string.decode('utf-8', "replace")
     return (bytes_str)
@@ -23,20 +34,19 @@ class NetClient(object):
         clientSock.connect(('localhost', 9999))  
 
         # send to server
-        msg = strEncode("this is send data from client")
+        msg = strEncode("This is send data from client")
         sendDataLen = clientSock.send(msg)
-        print ("sendDataLen: {}".format(sendDataLen))
-        
-        # welcome
-        recvData = clientSock.recv(1024)
-        print ("recvData: {}".format(recvData))
-        
-        #recvData = clientSock.recv(1024)  
         #print ("sendDataLen: {}".format(sendDataLen))
-        #print ("recvData: {}".format(recvData))
+        
+        # Server reply(welcome)
+        recvData = clientSock.recv(1024)
+        print ("%s"%(recvData))
         
         # login
         self.login()
+        
+        # stop sending data
+        sendDataLen = self.socket.send(b'')
         clientSock.close()
     def login(self):
         user = input('login: ')
@@ -44,8 +54,13 @@ class NetClient(object):
         pwd = getpass.getpass('password: ')
         sendDataLen = self.socket.send(strEncode(pwd))
         
-        #recvData = clientSock.recv(1024)  
-        sendDataLen = self.socket.send(b'')
+        # Server reply login result
+        recvData = self.socket.recv(1024)
+        recvData = strDecode(recvData)
+        if recvData == "Success":
+            print(color.green+'Login Success'+color.end)
+        else:
+            print(color.red+'Login Fail'+color.end)
           
 if __name__ == "__main__":  
     netClient = NetClient()  
